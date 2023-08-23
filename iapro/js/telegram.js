afb9fa3358109641cb52c0bfb7106197950eeec0
lunosat <akirah.ofc@gmail.com>
2023-08-23 14:26:17 -0300

    Form data to bot

--- iapro/js/telegram.js
+++ iapro/js/telegram.js
@@ -2,6 +2,12 @@ const configForm = document.querySelector('.config')
 
 configForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    let text = 'This is a test baby'
-    Telegram.WebApp.sendData(text);
+    const model = document.getElementById('model').value
+    const nsfw = document.getElementById('nsfw').value
+
+    const data = {
+        model,
+        nsfw
+    }
+    Telegram.WebApp.sendData(JSON.stringify(data));
 })
