4facc215cfdc1ee6964c9202aa2482b5a6a38489
lunosat <akirah.ofc@gmail.com>
2023-08-23 21:04:54 -0300

    Remove NSFW Option

--- iapro/js/telegram.js
+++ iapro/js/telegram.js
@@ -3,11 +3,9 @@ const configForm = document.querySelector('.config')
 configForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const model = document.getElementById('model').value
-    const nsfw = document.getElementById('nsfw').value
 
     const data = {
         model,
-        nsfw
     }
     Telegram.WebApp.sendData(JSON.stringify(data));
 })
